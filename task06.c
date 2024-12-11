a4d65cd38bd014ec6902155899b88458e6829aa0
djumanov <djumanovdev@gmail.com>
2024-12-11 09:37:04 +0500

    task06.c

--- task06.c
+++ task06.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-int main(){
-
-    char text[1000], word[1000];
-
-    printf("Matnni kiriting: ");
-    fgets(text, sizeof(text), stdin);
-    printf("So'zni kiriting: ");
-    scanf("%s", word);
-
-    if(strstr(text, word) != NULL){
-        printf("\"%s\" so'zi matn ichida bor\n", word);
-    } else {
-        printf("\"%s\" so'zi matn ichida yo'q\n", word);
+void find_word(char arr[], char arr2[])
+{ // Berilgan so’z char seriya ichida borligini tekshiruvchi funksiya
+    int count = 0, j = 0;
+    for (int i = 0; arr2[i+1] != '\0'; i++)
+    {
+        for (; arr[j] != '\0'; j++)
+        {
+            if (arr2[i] == arr[j] )
+            {
+                count++;
+                j++;
+                break;
+            }else
+            {
+                count = 0;
+                i = 0;
+            }
+        }    
     }
+    if (count == strlen(arr2)-1)
+    {
+        printf("Bu so`z seriyada mavjud: %s", arr2);
+        }else
+    {
+        printf("Bu shu seriyada mavju emas");
+    }
+}
 
-    return 0;
+int main()
+{
+    char arr[100], arr2[100];
+
+    printf("matnni kiriting: \n");
+
+    fgets(arr, sizeof(arr), stdin);
+
+    printf("so`zni kiriting: \n");
+
+    fgets(arr2, sizeof(arr2), stdin);
+
+    find_word(arr, arr2);
 }
